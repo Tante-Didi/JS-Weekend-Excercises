32c8561e36e295acf34a8cc573d4b0d98315cdb2
TanteDidi <danielarmoni@yahoo.com>
2024-05-04 17:01:39 +0300

    still in the first EXE

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 //Basic
 //1.EXE yes or No
+/*Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
+string for false
+*/
+
+/*1.understanding the problem:
+1.what does the function accepts? boolean
+2.what does the function returns ? a string.
+3.which string will she return? 
+*.The method function needs to return "Yes" as a string if the boolean value is true.
+*it should return "No" as a string if the boolean value is false.
+*/
+/*
+explore :
+1.declaring the method function
+2.checking the boolean value provided with if-else statement
+3.testing the method with console.log
+*/
